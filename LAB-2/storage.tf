de54e03dee03d8e38fa9650d2ed96436039ef2e9
Faisal Rehman <faisal.rehman22@hotmail.com>
2024-10-06 08:42:09 +0500

    Create storage.tf

---
+++ LAB-2/storage.tf
@@ -0,0 +1,26 @@
+resource "google_storage_bucket" "auto-expire" {
+  name          = var.bucket_name
+  storage_class = var.storage_class
+  location      = var.bucket_location
+  force_destroy = true
+  uniform_bucket_level_access = true
+
+   versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+
+resource "google_storage_bucket_object" "picture" {
+  name = "myimage"
+  bucket = "pizzaimages"
+  source = "myimage.png"  
+}
