22e81e6229a74c33c2b3c311a9ee9d2af71e3390
Faisal Rehman <faisal.rehman22@hotmail.com>
2024-10-16 12:53:03 +0500

    Create variables.tf

---
+++ terraform-modules-by-devopsmolvi/infra/storage/variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "East ASIA"
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "container_name" {
+  type = string
+}
