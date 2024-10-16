2bbcc0a59786f5e2fa2225402fecbbf05ef7a358
Faisal Rehman <faisal.rehman22@hotmail.com>
2024-10-16 12:52:13 +0500

    Create main.tf

---
+++ terraform-modules-by-devopsmolvi/infra/storage/main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "storage_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.storage_rg.name
+  location                 = azurerm_resource_group.storage_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
+}
